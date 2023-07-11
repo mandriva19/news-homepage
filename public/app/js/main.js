9fcc3219ab301bb6931f528078ad631a856f36ed
mandriva19 <97590591+mandriva19@users.noreply.github.com>
2023-07-11 11:14:41 +0400

    including public
    github didn't build my project without public folder

---
+++ public/app/js/main.js
@@ -0,0 +1,32 @@
+//get elements
+const menuOpen = document.getElementById("menuOpen");
+const menuClose = document.getElementById("menuClose");
+const mobileMenu = document.getElementById("mobileMenu");
+const mobileOverlay = document.getElementById("mobileOverlay");
+const body = document.querySelector("body");
+
+//open mobile-menu by clicking hamburger btn
+menuOpen.addEventListener("click", function () {
+  mobileMenu.classList.remove("translate-x-full");
+  mobileMenu.classList.remove("invisible");
+  mobileOverlay.classList.remove("hidden");
+  body.style.overflowY = "hidden";
+});
+
+//close menu by clicking close btn
+menuClose.addEventListener("click", function () {
+  mobileMenu.classList.add("translate-x-full");
+  mobileMenu.classList.add("invisible");
+  mobileOverlay.classList.add("hidden");
+  body.style.overflowY = "auto";
+});
+
+//close mobile Menu by clicking outside of it
+mobileOverlay.addEventListener("click", (event) => {
+  if (event.target === mobileOverlay) {
+    mobileOverlay.classList.add("hidden");
+    mobileMenu.classList.add("invisible");
+    mobileMenu.classList.add("translate-x-full");
+    body.style.overflowY = "auto";
+  }
+});
